8ea0e27e2ade2f3a73a17af7511b26aa8288423e
Nina <nina.bjaerum@skye.ey.com>
2024-02-07 09:22:17 +0100

    add authentication with nextAuth.js, chpt 15

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -4,6 +4,28 @@ import { z } from 'zod';
 import {sql} from '@vercel/postgres';
 import { revalidatePath } from 'next/cache'; 
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+){
+  try {
+    await signIn('credentials', formData);
+  } catch(error) {
+    if(error instanceof AuthError){
+      switch (error.type){
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
 
 const FormSchema = z.object({
     id: z.string(),
@@ -90,7 +112,7 @@ export async function updateInvoice(
       message: 'Missing Fields. Failed to Update Invoice.',
     };
   }
-  
+
   const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
  
